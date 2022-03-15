23a0d3629631f04ebaa3c512f568618ce352b72a
mrinmoymsarkar <msarkar07@live.in>
2022-03-15 11:48:59 +0530

    Update count.js
    tutorial way

--- count unqiue values/count.js
+++ count unqiue values/count.js
@@ -1,11 +1,27 @@
+// function countUniqueValues(params) {
+
+//     let counter = 0, count = {}
+
+//     for (let i of params) {
+//         count[i] =  count[i] ? count[i] +=1 : 1
+//     }
+//     return Object.keys(count).length
+// }
+
+// console.log(countUniqueValues([1,2,2,5,7,7,99]))
+
+//tutorial way
+
 function countUniqueValues(params) {
 
-    let counter = 0, count = {}
-
-    for (let i of params) {
-        count[i] =  count[i] ? count[i] +=1 : 1
+    let i = 0
+    for(let j = 1; j <params.length; j++) {
+        if(params[i] !== params[j]){
+            i++
+            params[i] = params[j]
+        }
     }
-    return Object.keys(count).length
+    return i+1
 }
 
 console.log(countUniqueValues([1,2,2,5,7,7,99]))
