86d564f01afcc1a27716f8628251c527b3e84c69
mrinmoymsarkar <msarkar07@live.in>
2022-03-21 23:43:48 +0530

    Create helperRecursive.js

---
+++ collect odd values/helperRecursive.js
@@ -0,0 +1,17 @@
+//collect odd values
+function oddValues(array) {
+    let odd = [];
+
+    function helper(array) {
+        if(array.length === 0) return;
+        else if(array[0]% 2 !== 0){
+            odd.push(array[0])
+        }
+        helper(array.slice(1))
+    }
+    helper(array)
+
+    return odd;
+}
+
+oddValues([1,2,3,4,5,6,7,8,9])
