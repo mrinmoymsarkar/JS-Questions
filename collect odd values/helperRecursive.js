c23b53f4e34dc3fffcde2e9f98a78e9d357212d3
mrinmoymsarkar <msarkar07@live.in>
2022-03-22 00:02:11 +0530

    Update helperRecursive.js

--- collect odd values/helperRecursive.js
+++ collect odd values/helperRecursive.js
@@ -1,4 +1,6 @@
 //collect odd values
+
+//helper
 function oddValues(array) {
     let odd = [];
 
@@ -10,6 +12,24 @@ function oddValues(array) {
         helper(array.slice(1))
     }
     helper(array)
+
+    return odd;
+}
+
+oddValues([1,2,3,4,5,6,7,8,9])
+
+
+//pure recursion
+//collect odd values
+function oddValues(array) {
+    let odd = [];
+    if(array.length === 0) return odd;
+        if(array[0]% 2 !== 0){
+            odd.push(array[0])
+        }
+        
+
+    odd = odd.concat(oddValues(array.slice(1)));
 
     return odd;
 }
